be59cc2e27ea3c2072d729d51e61d66d0b7c6a64
johnmart19 <johnivan19999@gmail.com>
2025-03-07 11:08:11 +0200

    Revert "sm8250-common: Switch to AIDL USB HAL"
    This reverts commit 163c68492e6ba04d1550893b2898827d05f11299.

--- kona.mk
+++ kona.mk
@@ -374,7 +374,7 @@ PRODUCT_PACKAGES += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb-service.qti
+    android.hardware.usb@1.3-service-qti
 
 PRODUCT_PACKAGES += \
     init.qcom.usb.rc \
